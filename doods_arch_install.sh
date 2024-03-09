7f2261a030f057af07067dc059c7d2c7f5944160
Doods Tecson <emmanuel.tecson@gmail.com>
2024-03-09 12:41:16 +0800

    Add files via upload

--- doods_arch_install.sh
+++ doods_arch_install.sh
@@ -29,6 +29,9 @@ git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && sudo pacman-key --lsign-key 3056513887B78AEB && sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf && sudo pacman -Syu --noconfirm && \
 #install yay packages
 yay -S --needed --noconfirm --sudoloop --removemake \
+stremio \
+pimpmystremio-bin \
+stremio-service-bin \
 rescuetime2 \
 stacer-bin \
 floorp-bin \
